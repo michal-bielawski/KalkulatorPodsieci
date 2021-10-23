cf858c23696a5040e868cabc291b78f05abeeb48
Michal <michalbielawski@o2.pl>
2021-10-23 23:12:16 +0200

    ipToArray

--- src/Kalkulator.java
+++ src/Kalkulator.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Kalkulator {
     public static void main(String[] args) {
         boolean koniec = true;
+        int ilePodsieci, ileHostow;
 
         Scanner in = new Scanner(System.in);
 
@@ -15,10 +16,21 @@ public class Kalkulator {
         char selectedOption = in.next().charAt(0);
 
         while (koniec) {
-            if (selectedOption == 'a' || selectedOption == 'b' || selectedOption == 'A' || selectedOption == 'B') {
-                Podsieci podsieci = new Podsieci(ip, selectedOption);
+            if (selectedOption == 'a' || selectedOption == 'A' ) {
+                System.out.println("Ile podsieci potrzebujesz?");
+                ilePodsieci = in.nextInt();
+
+                Podsieci podsieci = new Podsieci(ip, selectedOption, ilePodsieci);
                 koniec = false;
-            } else {
+            }
+            else if(selectedOption == 'b' || selectedOption == 'B' ){
+                System.out.println("Ile hostow potrzebujesz?");
+                ileHostow = in.nextInt();
+
+                Podsieci podsieci = new Podsieci(ip, selectedOption, ileHostow);
+                koniec = false;
+            }
+            else {
                 System.out.println("Błędnie wybrana opcja. Wybierz, czy chcesz obliczyć podsieci na daną :\n a)liczbę podsieci \n b)liczbę hostów");
                 selectedOption = in.next().charAt(0);
             }
--- src/Podsieci.java
+++ src/Podsieci.java
@@ -1,5 +1,8 @@
 public class Podsieci {
-    public Podsieci(String ip, char selectedOption){
+
+    private int[] oktetArray = new int[4];
+
+    public Podsieci(String ip, char selectedOption, int iloscPodzialu){
         if (selectedOption == 'a' || selectedOption == 'A'){
             System.out.println(checkKlasa(ip));
 
@@ -7,24 +10,28 @@ public class Podsieci {
     }
 
     private int checkKlasa(String ip){
-        String[] ipAll = ip.split("\\.");
-        int[] oktetArray = new int[4];
-        
-        for(int i = 0; i < ipAll.length; i++){
-            oktetArray[i] = Integer.parseInt(ipAll[i]);
-            if (oktetArray[i]>255){
-                return -1;
-            }
-        }
 
+        ipToArray(ip);
+
+        for (int i = 0; i<4; i++){
+            if(oktetArray[i] > 255) return -1;
+        }
 
         if(oktetArray[0] >= 1 && oktetArray[0] <= 127) return 8;
         else if (oktetArray[0] <= 191) return 16;
         else if (oktetArray[0] <= 223) return 24;
         else return -1;
 
+    }
+
+    private int[] ipToArray(String ip){
+        String[] ipAll = ip.split("\\.");
 
 
+        for(int i = 0; i < ipAll.length; i++){
+            oktetArray[i] = Integer.parseInt(ipAll[i]);
+        }
+        return oktetArray;
     }
 
 }
